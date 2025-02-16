47952b06a7e24d43e67174f5478e225a98aa5c5d
Akkdevs <akkdevs47@gmail.com>
2025-02-16 01:34:57 +0000

    test candidat complete

---
+++ bfem/database/test_canadidat.py
@@ -0,0 +1,29 @@
+from candidat import Candidat
+
+
+
+# on instancie l'interface de classe
+
+Interface_candidat =  Candidat()
+
+
+
+# on ajoute un candidat 
+
+# print(Interface_candidat.add_candidate("Modou","Ndiaye","21/12/2001","Dakar","H","Senegalais","0","Neutre","Gainde fatma","False"))
+
+# print(Interface_candidat.add_candidate("Modou","Gaye","21/12/2001","Dakar","H","Senegalais","0","Neutre","Gainde fatma","False"))
+
+
+# on recupre les candidat 
+
+# print(Interface_candidat.get_all_candidate())
+
+print(Interface_candidat.get_candidate(1))
+
+# on modifie le candidat numero 2
+
+# Interface_candidat.update_candidate(1,prenom="cana")
+
+
+# print(Interface_candidat.get_candidate(2))

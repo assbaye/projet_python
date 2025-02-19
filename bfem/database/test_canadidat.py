34ac5b5165f44471bc254fbc0038d0839d32bbef
Akkdevs <akkdevs47@gmail.com>
2025-02-19 22:13:51 +0000

    verifie releves non test

--- bfem/database/test_canadidat.py
+++ bfem/database/test_canadidat.py
@@ -10,14 +10,14 @@ Interface_candidat =  Candidat()
 
 # on ajoute un candidat 
 
-# print(Interface_candidat.add_candidate("Modou","Ndiaye","21/12/2001","Dakar","H","Senegalais","0","Neutre","Gainde fatma","False"))
+print(Interface_candidat.add_candidate("Modou","Ndiaye","21/12/2001","Dakar","H","Senegalais","0","Neutre","Gainde fatma","False"))
 
-# print(Interface_candidat.add_candidate("Modou","Gaye","21/12/2001","Dakar","H","Senegalais","0","Neutre","Gainde fatma","False"))
+print(Interface_candidat.add_candidate("Modou","Gaye","21/12/2001","Dakar","H","Senegalais","0","Neutre","Gainde fatma","False"))
 
 
 # on recupre les candidat 
 
-# print(Interface_candidat.get_all_candidate())
+print(Interface_candidat.get_all_candidate())
 
 print(Interface_candidat.get_candidate(1))
 

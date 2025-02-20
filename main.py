17517de00738d2bcb9fb22591bb8d741ba29d173
assbaye <assbaye123@gmail.com>
2025-02-20 21:32:36 +0000

    Résolution des conflits

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 from bfem.template.MyApp import MyApp
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    MyApp().run() 
-=======
     MyApp().run()
-    
-
-
->>>>>>> 07ac65c64bac2459eb80dce905d634aafce872dc
